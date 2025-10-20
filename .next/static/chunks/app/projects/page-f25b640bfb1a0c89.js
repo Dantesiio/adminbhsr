a7673497332e2d60792b8010a50e14e0dd8f44f7
dantesiio <daviddo29@hotmail.com>
2025-10-20 18:32:35 -0500

    chore: exclude .next/ and build artifacts from git
    - Add comprehensive .gitignore
    - Remove .next/ directory from version control
    - Remove .DS_Store
    - Reduce repo size and follow Next.js best practices

--- .next/static/chunks/app/projects/page-f25b640bfb1a0c89.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[895,822,871,597],{3677:function(e,n,s){Promise.resolve().then(s.t.bind(s,6612,23)),Promise.resolve().then(s.bind(s,9045))}},function(e){e.O(0,[747,687,45,580,996,744],function(){return e(e.s=3677)}),_N_E=e.O()}]);
